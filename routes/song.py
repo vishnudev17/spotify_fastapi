8c807568099d49c049bd75eaa6526734ed7861ff
vishnudev17 <vishnudev17>
2025-07-25 12:58:31 +0530

    latest commit

--- routes/song.py
+++ routes/song.py
@@ -50,3 +50,11 @@ def upload_song(
     db.commit()
     db.refresh(new_song)
     return new_song
+
+
+@router.get("/list")
+def list_songs(db: Session = Depends(get_db), auth_details=Depends(auth_middleware)):
+    songs=db.query(Song).all()
+    return songs
+
+    pass
